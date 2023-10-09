ecf9bcc398a0f11a70b44ce7506ce186125f2311
I551990 <constantin.heeren@sap.com>
2023-10-09 14:03:09 +0200

    scrape position struct and add map

--- src/server/game.cpp
+++ src/server/game.cpp
@@ -6,13 +6,10 @@ void Game::start()
     cout << colorize("Game started", color::green) + " " + colorize("[" + std::to_string(width) + "x" + std::to_string(height) + "]", color::blue) << endl;
 
     // Initialize player positions
-    for (Player &player : *players)
-    {
-        Player::position pos;
-        pos.x = rand() % width;
-        pos.y = rand() % height;
-        player.setPos(pos);
-    }
+    // for (Player &player : *players)
+    // {
+        cout << colorize("Player initialization not implemented", color::red) << endl;
+    // }
 
     // Start game loop
     gameLoop();
@@ -30,7 +27,7 @@ void Game::gameLoop()
             {
                 continue;
             }
-            m_server->sendPacketToAllPlayers(ServerPacketType::pos, {player.getName(), std::to_string(player.getPos().x), std::to_string(player.getPos().y)});
+            cout << colorize("Pos packet not implemented", color::red) << endl;
         }
 
         // sleep for 1 second
@@ -47,38 +44,12 @@ void Game::gameLoop()
                 continue;
             }
             // move player
-            Player::position newPos = player.getPos();
-            switch (player.getNextMove())
-            {
-            case move::UP:
-                newPos.y--;
-                break;
-            case move::DOWN:
-                newPos.y++;
-                break;
-            case move::LEFT:
-                newPos.x--;
-                break;
-            case move::RIGHT:
-                newPos.x++;
-                break;
-            default:
-                break;
-            }
+            cout << colorize("player movement not implemented", color::red) << endl;
 
             // check for collision
-            if (newPos.x < 0 || newPos.x >= width || newPos.y < 0 || newPos.y >= height)
-            {
-                // out of bounds
-                // kill player
-                diedPlayers.push_back(&player);
-                player.setIsAlive(false);
-
-                continue;
-            }
+            
 
             // update player position
-            player.setPos(newPos);
         }
 
         // send game tick
